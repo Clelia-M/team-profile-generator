e1f1ac65f1e15e0d57ca459a9ae798fd966da7f1
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-02-24 18:37:45 +0000

    pseudocode for the index js

--- index.js
+++ index.js
@@ -13,3 +13,44 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+inquirer.prompt([{
+    //manager questions
+}]).then(response => {
+    // populate manager info
+    // promptForNexEmployee ()
+})
+
+const promptForNextEmployee = () => {
+    inquirer.prompt([{
+        // choice of 3
+    }]).then(response => {
+        // if engineer
+        //    promptForEngineer
+        // else if intern
+        //    promptForIntern
+        // else
+        //    use the functionality from page-template to generate the team
+    })
+}
+
+const promptForEngineer = () => {
+    inquirer.prompt([{
+        //engineer questions
+    }]).then(response => {
+        // add new engineer to employees array
+        // promptForNextEmployee
+    })
+}
+
+const promptForIntern = () => {
+    inquirer.prompt([{
+        //intern questions
+    }]).then(response => {
+        // add new intern to employees array
+        // promptForNextEmployee
+    })
+}
+
+const buildPage = () => {
+// render(myArrayOfTeamMembers)
+}
